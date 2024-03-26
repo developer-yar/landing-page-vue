e302a0f469733d60de0f6a52d69084c061d11f7a
yaroslav <yaroslavdvorski@gmail.com>
2024-03-26 10:32:47 +0300

    chore(*): init app

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="footer">
+    <SectionLabelText text="©2015-2024 Company Name" />
+  </footer>
+</template>
+
+<script setup lang="ts">
+import SectionLabelText from "@/components/SectionLabelText.vue";
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  background-color: var(--dark-gray);
+  block-size: 125px;
+  display: grid;
+  place-content: center;
+}
+</style>
